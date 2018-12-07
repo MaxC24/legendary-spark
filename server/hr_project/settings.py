780c9185fdbf6e2fb3c65c67c5bff2503a4f0fd9
Massimo Crapanzano <massimo.crapanzano@gmail.com>
2018-12-06 20:09:48 -0600

    adds frontend to the app

--- server/hr_project/settings.py
+++ server/hr_project/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -43,8 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'hungry_pets',
     'corsheaders',
+    'hungry_pets',
+    'frontend',
 ]
 
 MIDDLEWARE = [
@@ -137,5 +137,6 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'public'),
+]
--- server/hr_project/urls.py
+++ server/hr_project/urls.py
@@ -23,7 +23,10 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('hungry_pets.urls')),
+    path('', include('frontend.urls')),
 ]
 
 # Adding the image folder as static one:
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
