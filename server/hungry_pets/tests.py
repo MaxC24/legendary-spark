4c160673336c7e516d9c42e34fdd2109273c1030
Massimo Crapanzano <massimo.crapanzano@gmail.com>
2019-03-05 11:49:53 -0500

    adds a testcase

--- server/hungry_pets/tests.py
+++ server/hungry_pets/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from hungry_pets.models import Pet
 
 # Create your tests here.
+
+class PetTests(TestCase):
+
+    def test_pet_model(self):
+        """
+        True is True
+        """
+        self.assertIs(True, True)
+
